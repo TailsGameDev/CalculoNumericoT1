adf5c079e1799837ab4e42d15bb58e6a726c05aa
caina <cainacc@hotmail.com>
2019-04-14 17:19:03 -0300

    vets tridiagonais montados

--- main.m
+++ main.m
@@ -1,15 +1,16 @@
 format long
 
-n = 10;
+n = 5;
 
-%1a)
+%a)
 [a b] = montaAB(n);
 
 printf('a2)\n');
+%mostrar x
 [x nop] = fLUCrout(n,a,b);
 printf('x(1) = %.16f\n', x(1));
 printf('x(n) = %.16f\n', x(n));
-%res
+%residuos
 r = a * transpose(x) - transpose(b);
 residuoMaximo = max(abs(r))
 
@@ -17,3 +18,7 @@ printf('a3)\n');
 printf('Numero de operacoes medido: %d\n', nop);
 nopt = (4*n^3 + 15*n^2 - 7*n - 6) / 6;
 printf('Numero de operacoes teorico: %d\n', nopt);
+
+%b
+%b1)
+[t r d] = montaVetsTrd(a,n);
---
+++ montaVetsTrd.m
@@ -0,0 +1,15 @@
+function [t r d] = montaVetsTrd(a,n)
+  i=1;
+  a
+  r(i) = a(1,1); d(i) = a(1,2);
+
+  for i=2:n-1
+    t(i) = a(i,i-1); r(i) = a(i,i); d(i) = a(i,i+1);
+  end
+
+  i=n;
+  t(i) = a(i,i-1); r(i) = a(i,i);
+
+  t(1) = NaN; d(n) = NaN;
+
+end
